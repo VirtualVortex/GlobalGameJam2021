8c0ebc5feba15572c9c8eb338994f7bc2a7b7d06
VirtualVortex <mcoates111@gmail.com>
2021-01-29 18:13:35 +0000

    Adding player movement to the project.
    Signed-off-by: VirtualVortex <mcoates111@gmail.com>

---
+++ GlobalGamejam20201/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public FloatVariable speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.up * y;
+        move *= speed.value;
+
+        rb.velocity = move;
+    }
+}
