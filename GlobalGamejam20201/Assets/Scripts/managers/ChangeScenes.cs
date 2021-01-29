fa6896f026e220e2da957189d11893181c8d954e
VirtualVortex <mcoates111@gmail.com>
2021-01-29 21:19:18 +0000

    Adding scene transition.
    Signed-off-by: VirtualVortex <mcoates111@gmail.com>

---
+++ GlobalGamejam20201/Assets/Scripts/managers/ChangeScenes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("Player")) ChangeScene();
+    }
+}
