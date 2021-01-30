289ec160db660d15d99ac3ff4190b30a8ac567f9
AC215888 <ac215888@falmouth.ac.uk>
2021-01-30 20:32:18 +0000

    Added the settings menus, and replaced the assets for the rhythm game

--- GlobalGamejam20201/Assets/Scripts/UI/Settings.cs
+++ GlobalGamejam20201/Assets/Scripts/UI/Settings.cs
@@ -1,18 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
+
+    public Slider SFXSLIDER;
+
+    public Slider MusicSlider;
+
+    public AudioMixer audioMixerMusic;
+
+
+
     void Start()
     {
-        
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(Option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = CurrentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        SFXSLIDER.value = PlayerPrefs.GetFloat("volume", 1f);
+
+        audioMixer.SetFloat("volume", Mathf.Log10(SFXSLIDER.value) * 20);
+
+        MusicSlider.value = PlayerPrefs.GetFloat("volumeMusic", 1f);
+
+        audioMixerMusic.SetFloat("volumeMusic", Mathf.Log10(SFXSLIDER.value) * 20);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float volume)
     {
-        
+        PlayerPrefs.SetFloat("volume", volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
     }
+
+    public void SetVolumeMusic(float volumeMusic)
+    {
+        PlayerPrefs.SetFloat("volume", volumeMusic);
+        audioMixerMusic.SetFloat("volumeMusic", Mathf.Log10(volumeMusic) * 20);
+    }
+
+
+    public void SetResolution(int resolutionsIndex)
+    {
+        Resolution resolution = resolutions[resolutionsIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
 }
