8bbced442faea8a251b9492395be02aadf100f7b
AC215888 <ac215888@falmouth.ac.uk>
2021-01-30 22:28:19 +0000

    Added and created a audio manager, and two mixers for the different sliders

--- GlobalGamejam20201/Assets/Scripts/managers/AudioManager.cs
+++ GlobalGamejam20201/Assets/Scripts/managers/AudioManager.cs
@@ -1,18 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public sound[] sounds;
+
+    public static AudioManager instance;
+
+    public AudioMixer SFXVolume;
+
+    public AudioMixer MusicVolume;
+
+    void Awake()
     {
-        
+
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        foreach (sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            //s.source.volume = PlayerPrefs.GetFloat("volume", 1f);
+            //s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+            s.source.outputAudioMixerGroup = s.audioMixer;
+
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        SFXVolume.SetFloat("volume", Mathf.Log10(PlayerPrefs.GetFloat("volume", 1)) * 20);
+        //Play("Wind_1");
+        //Play("Bad_Weather");
+        //Play("Grass");
+        //Play("Bird_1_Sound_1");
+    }
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("sound: " + name + " not found! ");
+            return;
+        }
+
+        s.source.Play();
     }
 }
---
+++ GlobalGamejam20201/Assets/Scripts/managers/sound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class sound
+{
+        public string name;
+
+        public AudioClip clip;
+
+
+        //public float volume;
+        //[Range(0.1f, 3f)]
+        //public float pitch;
+
+        public AudioMixerGroup audioMixer;
+
+        public bool loop;
+
+        [HideInInspector]
+        public AudioSource source;
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    
+}
