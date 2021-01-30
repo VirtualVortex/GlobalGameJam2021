7790dae3d74d721d9ecf88d6225a4ddcafbfd8f9
VirtualVortex <mcoates111@gmail.com>
2021-01-30 12:36:30 +0000

    Working on item checker for door.
    Signed-off-by: VirtualVortex <mcoates111@gmail.com>

---
+++ GlobalGamejam20201/Assets/Scripts/managers/ItemChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChecker : MonoBehaviour
+{
+    ChangeScenes cs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = GetComponent<ChangeScenes>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("Player"))
+        {
+            if (collision.GetComponent<InventrySystem>().inventory.Count >= 3)
+                cs.ChangeScene();
+        }
+    }
+}
