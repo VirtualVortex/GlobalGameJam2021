71ab74be235d2b37f151a30b5da33fd4414c001c
VirtualVortex <mcoates111@gmail.com>
2021-01-30 18:08:56 +0000

    Added BeaconManager.cs
    Signed-off-by: VirtualVortex <mcoates111@gmail.com>

---
+++ GlobalGamejam20201/Assets/Scripts/managers/BeaconManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconManager : MonoBehaviour
+{
+    [SerializeField] AudioSource[] audioSources;
+
+    int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = PlayerPrefs.GetInt("saveNum");
+        SwitchAudio();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchAudio()
+    {
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            if (i == number)
+                audioSources[number].enabled = true;
+
+            if (i != number)
+                audioSources[i].enabled = false;
+        }
+    }
+
+    public void SaveNumber()
+    {
+        PlayerPrefs.SetInt("saveNum", number + 1);
+    }
+}
